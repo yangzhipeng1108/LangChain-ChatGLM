ef22f0a40232b59cd6e62407be956f9b13b5399b
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-05-30 08:52:59 +0800

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    llm_model_name = 'THUDM/chatglm-6b'  # 本地模型文件 or huggingface远程仓库
-    embedding_model_name = 'GanymedeNil/text2vec-large-chinese'  # 检索模型文件 or huggingface远程仓库
-    vector_store_path = 'resource/faiss/'
-    docs_path = 'resource/txt/'
